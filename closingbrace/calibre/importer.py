9201b84ba1147b32b426b65ca55ca1a2a593ee42
Hans Vredeveld <github@closingbrace.nl>
2019-08-23 18:14:19 +0200

    Add parsing of the application's command line

--- closingbrace/calibre/importer.py
+++ closingbrace/calibre/importer.py
@@ -7,6 +7,26 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import argparse
+
+from os.path import expanduser
+
+
+def parse_command_line():
+    """Parse command line arguments.
+    """
+    parser = argparse.ArgumentParser(
+            description="Import magazines into Calibre")
+    parser.add_argument("-c", "--config",
+            default=expanduser("~/.calibre-magazine-importer"),
+            help="configuration file for the importer (default: "
+            "$HOME/.calibre-magazine-importer)")
+    parser.add_argument("-v", "--verbose", help="be more verbose about "
+            "the magazines that are imported", action="store_true")
+    return parser.parse_args()
+
+
 def run():
     """Run the importer application."""
-    pass
+    cmd_line = parse_command_line()
+    print(cmd_line.config)
